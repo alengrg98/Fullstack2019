11a698622f45563848d206786df8f4534751a1cb
alengrg98 <alengeorgeshibu@cs.ajce.in>
2019-07-24 15:27:28 +0530

    javascript until forloop

--- htmlcss/js/test.js
+++ htmlcss/js/test.js
@@ -1,8 +1,9 @@
 var x=10;           //var creates global variables
 if(x<15)
 {
-    let x=20;               //let creates a local variable
+    let x=20;               //let creates a local variable and const is used to create constants
     x=x+1;
     console.log(x);
 }
 console.log(x);
+
